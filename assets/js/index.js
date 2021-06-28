e00b744c663e244aee3a818bbe6e48f5dec97f0a
Carlos Chang <carloschang51@yahoo.com>
2021-06-28 13:15:52 -0300

    proyecto termiando

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,38 @@
-const ACERCA = document.querySelector('acerca')
-const SECTION_ACERCA = document.getElementById('#acerca')
+// constantes para clases
+const containerNav = document.querySelector('.container-nav');
+
+// constantes para id
+const open = document.getElementById('open');
+const nav = document.getElementById('nav');
 
 
 
 
-ACERCA.onclick = () => {
-    return acerca()
+// esta funcion remueve el display none
+const removeHide = () => {
+
+    containerNav.classList.remove('hide');
+
 }
+
+// esta funcion agrega el display none
+const addHide = () => {
+
+    containerNav.classList.add('hide');
+
+}
+
+
+// funcion de menu de hamburguesa
+
+open.addEventListener('click', () => {
+
+    if (document.querySelector('.hide')) {
+        removeHide();
+
+    } else {
+        addHide();
+
+    }
+
+})
